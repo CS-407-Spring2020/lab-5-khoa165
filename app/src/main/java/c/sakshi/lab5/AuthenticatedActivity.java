1a8d6e42f5b09c6b4e81d3c56ebc5f5c74debf0f
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-03-01 23:04:43 -0600

    add menu, store username in sharePreferenes and redirect screen if login, logout clears it

--- app/src/main/java/c/sakshi/lab5/AuthenticatedActivity.java
+++ app/src/main/java/c/sakshi/lab5/AuthenticatedActivity.java
@@ -1,9 +1,15 @@
 package c.sakshi.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class AuthenticatedActivity extends AppCompatActivity {
@@ -15,8 +21,42 @@ public class AuthenticatedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_authenticated);
 
         welcomeText = (TextView) findViewById(R.id.welcomeText);
-        Intent intent = getIntent();
-        String str = intent.getStringExtra("name");
-        welcomeText.setText("Welcome " + str + "!");
+
+        // Get username from intent, this approach is still valid.
+        // Intent intent = getIntent();
+        // String username = intent.getStringExtra("username");
+
+        // Get username from sharedPreferences.
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+
+        welcomeText.setText("Welcome " + username + "!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            // Remove username form sharedPreferences.
+            SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+            sharedPreferences.edit().remove("username").apply();
+
+            // Return to login screen.
+            goToLoginScreen();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goToLoginScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -2,7 +2,9 @@ package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,18 +14,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+
+        String username = sharedPreferences.getString("username", "");
+        if (username != null && !username.equals("")) {
+            goToAuthenticatedScreen(username);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 
     public void login(View view) {
-        EditText nameInput = (EditText) findViewById(R.id.nameInput);
-        String name = nameInput.getText().toString();
-        goToAuthenticatedScreen(name);
+        // Get username and password.
+        EditText usernameInput = (EditText) findViewById(R.id.usernameInput);
+        String username = usernameInput.getText().toString();
+        EditText passwordInput = (EditText) findViewById(R.id.passwordInput);
+        String password = passwordInput.getText().toString();
+
+        // Go to authenticated screen.
+        goToAuthenticatedScreen(username);
     }
 
-    public void goToAuthenticatedScreen(String s) {
+    public void goToAuthenticatedScreen(String username) {
+        // Add username to SharedPreferences object.
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", username).apply();
+
+        // Pass data
         Intent intent = new Intent(this, AuthenticatedActivity.class);
-        intent.putExtra("name", s);
+        intent.putExtra("username", username);
         startActivity(intent);
     }
 }
